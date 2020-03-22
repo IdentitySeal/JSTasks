4c990411907d6b5c2e07d040fd5915e8a55fc115
IdentitySeal <teemealeheen@gmail.com>
2020-03-22 21:30:21 +0100

    List of Odd Numbers

---
+++ index.js
@@ -0,0 +1,14 @@
+var name = "IdentitySeal";
+var courses_offered = ["HTML","CSS","JAVASCRIPT","NODE.JS","PYTHON"];
+alert(name + ' says Welcome');
+console.log(name + " offers " + courses_offered.length + " courses, which are: " + courses_offered);
+console.log("List of odd numbers from 1-200")
+var i;
+
+
+ for(i = 1; i <= 200; i++){
+   if(!(i % 2) == 0){
+   	console.log(i);
+    
+   }
+}
